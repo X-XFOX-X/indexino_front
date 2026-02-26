165b93ad7c989d73ebf7e6fe12e7900653a5b6e9
Your Name <you@example.com>
2026-02-26 12:45:22 +0330

    stert tailwind

---
+++ tailwind.config copy.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{html,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1Ee0FF',
+      }
+    },
+  },
+  plugins: [],
+}
